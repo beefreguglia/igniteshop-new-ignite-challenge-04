91c03d03b61bf1b8323374abc9319fa792dc3ad9
bernardo <beefreguglia@gmail.com~>
2022-12-28 01:14:55 -0300

    linting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from 'next/app'
-import Image from 'next/image';
+import Image from 'next/image'
 import { globalStyles } from '../styles/global'
 
 import logoImg from '../assets/logo.svg'
-import { Container, Header } from '../styles/pages/app';
+import { Container, Header } from '../styles/pages/app'
 
-globalStyles();
+globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {  
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
