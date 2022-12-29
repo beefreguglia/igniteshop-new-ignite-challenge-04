7b10226458c51285cd461cf7e0de61c96b521f77
bernardo <beefreguglia@gmail.com~>
2022-12-28 23:14:47 -0300

    create product interface

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,27 @@
+/* eslint-disable no-unused-vars */
+// import { useRouter } from 'next/router'
+import {
+  ImageProductContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+
 export default function Product() {
-  return <h1>Product</h1>
+  // const { query } = useRouter()
+  return (
+    <ProductContainer>
+      <ImageProductContainer></ImageProductContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79.90</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+          labore vitae provident quaerat. Possimus officiis nemo iusto veniam
+          sint corrupti dolores excepturi. Cumque vero dignissimos inventore
+          quisquam hic unde sint.
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
