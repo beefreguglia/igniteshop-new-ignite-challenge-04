f99f609dc9b56768b28f126f2704e1a232b549ce
bernardo <beefreguglia@gmail.com~>
2023-01-08 14:50:46 -0300

    Toast configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { Header } from '../components/Header'
 import { CartContextProvider, useCart } from '../hooks/useCart'
 import { globalStyles } from '../styles/global'
 import { Container } from '../styles/pages/app'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 globalStyles()
 
@@ -13,6 +15,22 @@ export default function App({ Component, pageProps }: AppProps) {
         <Header />
         <Component {...pageProps} />
       </Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        style={{
+          marginTop: '2rem',
+          backgroundColor: '#121214'
+        }}
+      />
     </CartContextProvider>
   )
 }
